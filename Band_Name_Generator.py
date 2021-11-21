79ce3be24160491e9211c08ec418de4110a92a70
John Adams <john.c.adams32@gmail.com>
2021-11-21 13:18:28 -0700

    Finished Project

---
+++ Band_Name_Generator.py
@@ -0,0 +1,14 @@
+# This is my first program in the 100 days of code challenge
+# This program generates a band name given user input of a city and pets name.
+
+# Welcome Statement
+print("Welcome to the Band Name Generator\n")
+
+# Ask the user for the city they were born in.
+cityname = input("What is the name of the city you grew up in?\n")
+
+# Ask the user the name of their pet
+petname = input("What is your pets name?\n")
+
+# Print the users new band name
+print("Your new band name is " + cityname + " " + petname)
